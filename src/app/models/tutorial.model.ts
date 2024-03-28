6abb9494051995d03b5820202726f2473ae7eadf
jhonn <jhonn.sotomayor@vallegrande.edu.pe>
2024-03-28 15:12:21 -0500

    Eliminar todos los archivos excepto README.md de la rama main

--- src/app/models/tutorial.model.ts
+++
@@ -1,7 +0,0 @@
-export class Tutorial {
-  key?: string | null;
-  nombre?: string;
-  apellidos?: string;
-  rol?: string;
-  published?: boolean;
-}
--- src/app/models/users.model.ts
+++
@@ -1,5 +0,0 @@
-export class Users {
-   email?: string;
-   name?: string;
-   rol?: string;
- }
