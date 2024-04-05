adb92ced93b6c5e3908a962043c2804eebc037aa
PedroLuyo <pedro.porras@vallegrande.edu.pe>
2024-04-05 09:00:09 -0500

    add component platos v1.1

--- src/app/components/platos/platos.component.ts
+++ src/app/components/platos/platos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlatosService } from '../../services/platos.service';
 
 @Component({
   selector: 'app-platos',
   templateUrl: './platos.component.html',
-  styleUrl: './platos.component.css'
+  styleUrls: ['./platos.component.css']
 })
-export class PlatosComponent {
+export class PlatosComponent implements OnInit {
 
+  platos: any[] = [];
+
+  constructor(private platosService: PlatosService) { }
+
+  ngOnInit(): void {
+    this.loadPlatos();
+  }
+
+  private loadPlatos(): void {
+    this.platosService.getPlatos().subscribe(
+      platos => {
+        this.platos = platos;
+      },
+      error => {
+        console.error('Error fetching platos:', error);
+      }
+    );
+  }
 }
