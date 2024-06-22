708fce4c0f65028949a70dc216a838b5d01abca6
AngeloLaMadrid <angelo.lamadrid@vallegrande.edu.pe>
2024-06-21 19:55:12 -0500

    ruta api secundaria para platoCarta

--- src/app/services/platocarta.service.ts
+++ src/app/services/platocarta.service.ts
@@ -1,19 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlatocartaService {
 
-  //apiUrl = 'https://8086-vallegrande-msrestauran-jr8yds39xxz.ws-us114.gitpod.io/api/v1/products/obtener';
-  private apiUrl = 'http://service-ms-menurestaurant:8086/api/v1/products/obtener'; 
+  private ApiUrl = 'http://localhost:8086/api/v1/products/obtener';
+  private ApiUrlAngelo = 'http://localhost:30002/api/v1/products/obtener';
   
   constructor(private http: HttpClient) { }
 
   getPlatosCarta(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.ApiUrl).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // Si falla la primera URL, intenta con la segunda
+        console.error(`Error al conectar con la API primaria: ${error.message}. Intentando con la API de Angelo.`);
+        return this.http.get<any>(this.ApiUrlAngelo);
+      })
+    );  
   }
 
   logPlatosCarta(): void {
@@ -21,6 +28,12 @@ export class PlatocartaService {
   }
 
   getFilteredPlatos(searchTerm: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?search=${searchTerm}`);
-  }
+    return this.http.get<any>(`${this.ApiUrl}?search=${searchTerm}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // Si falla la primera URL, intenta con la segunda
+        console.error(`Error al conectar con la API primaria: ${error.message}. Intentando con la API de Angelo.`);
+        return this.http.get<any>(`${this.ApiUrlAngelo}?search=${searchTerm}`);
+      })
+    );
+  } 
 }
