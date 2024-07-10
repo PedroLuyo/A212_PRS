26fc9701070c21c88b0ccfe0821b21c40af0bd6b
jeferson.mauricio <jeferson.mauricio@vallegrande.edu.pe>
2024-07-10 00:55:28 -0500

    standar de reporte platocarta

--- src/app/components/platocarta/productos/productos.component.ts
+++ src/app/components/platocarta/productos/productos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, NgForm } from '@angular/forms';
 import Swal from 'sweetalert2';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 
 declare var $: any;
@@ -50,7 +52,97 @@ export class ProductosComponent implements OnInit{
     this.presentacionControl.setValue(this.presentaciones[0]);
   }
 
-  
+  generarReportePDF(): void {
+    const doc = new jsPDF({
+      orientation: 'landscape'
+    });
+
+    const img = new Image();
+    img.src = 'assets/img/Logo Transparente Gastro Connect.png';
+    img.onload = () => {
+      const pageWidth = doc.internal.pageSize.getWidth();
+      const pageHeight = doc.internal.pageSize.getHeight();
+      const logoWidth = pageWidth * 0.2;
+      const logoHeight = img.height * (logoWidth / img.width);
+      const logoX = (pageWidth - logoWidth) / 2;
+      doc.addImage(img, 'PNG', logoX, 10, logoWidth, logoHeight);
+
+      const slogan = "Disfruta de la mejor gastronomía con Gastro Connect";
+      const sloganX = pageWidth / 2;
+      const sloganY = logoHeight + 20;
+      doc.setTextColor(31, 30, 30);
+      doc.setFontSize(12);
+      doc.text(slogan, sloganX, sloganY, { align: 'center' });
+
+      const fecha = new Date().toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+      }).replace(/ /g, '/').replace(/\//g, '-');
+
+      doc.setFont('courier', 'bold');
+      doc.setFontSize(20);
+      const titulo = 'Reporte de Productos';
+      const tituloY = sloganY + 20;
+      doc.text(titulo, 14, tituloY);
+
+      doc.setFontSize(12);
+      const fechaX = pageWidth - 14;
+      doc.text(`Fecha: ${fecha}`, fechaX, tituloY, { align: 'right' });
+
+      const head = [['ID', 'Nombre', 'Descripción', 'Precio', 'Categoría', 'Presentación', 'Stock', 'Estado']];
+      const data = this.platos.map((plato) => [
+        plato.id,
+        plato.nombre,
+        plato.descripcion,
+        plato.precio,
+        this.getNombreCategoria(plato.id_categoria),
+        this.getTipoPresentacion(plato.id_presentacion),
+        plato.stock,
+        plato.estado === 'A' ? 'Activo' : 'Inactivo'
+      ]);
+
+      (doc as any).autoTable({
+        head: head,
+        body: data,
+        startY: tituloY + 20,
+        styles: {
+          cellWidth: 'auto',
+          fontSize: 10,
+          lineColor: [0, 0, 0],
+          lineWidth: 0.1
+        },
+        headStyles: {
+          fillColor: [0, 0, 0],
+          textColor: 255,
+          fontStyle: 'bold'
+        },
+        bodyStyles: {
+          fillColor: [255, 255, 255],
+          textColor: 0
+        },
+        alternateRowStyles: {
+          fillColor: [235, 235, 235]
+        }
+      });
+
+      const pageCount = doc.getNumberOfPages();
+      for (let i = 1; i <= pageCount; i++) {
+        doc.setPage(i);
+        doc.setFont('courier', 'normal');
+        doc.setFontSize(10);
+        doc.setTextColor(31, 30, 30);
+        const pageNumberText = `Página ${i}`;
+        const pageSize = doc.internal.pageSize;
+        const pageWidth = pageSize.getWidth();
+        const pageHeight = pageSize.getHeight();
+        const footerY = pageHeight - 10;
+        doc.text(pageNumberText, pageWidth - doc.getTextWidth(pageNumberText) - 10, footerY);
+      }
+
+      doc.save('reporte_productos.pdf');
+    };
+  }
 
   getNombreCategoria(idCategoria: number): string {
     const categoria = this.categorias.find(c => c.id === idCategoria);
