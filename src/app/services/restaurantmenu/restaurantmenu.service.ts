dfa7b068f2256e850516597354d7cb83c9b3b2b9
AngeloLaMadrid <angelo.lamadrid@vallegrande.edu.pe>
2024-07-11 11:01:58 -0500

    chore: Actualizar URL de la API del servicio de menú del restaurante

--- src/app/services/restaurantmenu/restaurantmenu.service.ts
+++ src/app/services/restaurantmenu/restaurantmenu.service.ts
@@ -9,7 +9,7 @@ import { catchError } from 'rxjs/operators';
 export class RestauranteMenuService {
 
   private apiUrl = 'https://8086-vallegrande-msrestauran-zp5gu5cznrm.ws-us115.gitpod.io/api/v1/products';
-  private apiUrlAngelo = 'http://localhost:8086/api/v1/products';
+  private apiUrlAngelo = 'http://menurestaurant-service:8086/api/v1/products';
   
   constructor(private http: HttpClient) { }
 
