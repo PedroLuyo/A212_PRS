05cdf5a35322ab296adb7353f2358f6bc1e691d7
Angelo <angelo.lamadrid@vallegrande.edu.pe>
2024-07-13 08:37:04 -0500

    fix

--- src/app/services/restaurantmenu/restaurantmenu.service.ts
+++ src/app/services/restaurantmenu/restaurantmenu.service.ts
@@ -9,7 +9,7 @@ import { catchError } from 'rxjs/operators';
 export class RestauranteMenuService {
 
   private apiUrl = 'http://localhost:8086/api/v1/products';
-  private apiUrlAngelo = 'http://35.192.163.133:8086/api/v1/products/obtener';
+  private apiUrlAngelo = 'http://35.188.205.186:8086/api/v1/products';
   
   constructor(private http: HttpClient) { }
 
